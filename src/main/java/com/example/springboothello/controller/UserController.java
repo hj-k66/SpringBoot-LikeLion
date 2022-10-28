74799893ab4c250c572efdc0db494cc0eb000af2
heejeong <hjx66@naver.com>
2022-10-28 13:52:51 +0900

    [feat] UserController deleteAll() 구현

--- src/main/java/com/example/springboothello/controller/UserController.java
+++ src/main/java/com/example/springboothello/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.springboothello.controller;
 
 import com.example.springboothello.dao.UserDao;
 import com.example.springboothello.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,15 @@ public class UserController {
         this.userDao = userDao;
     }
 
-    @PostMapping("/user")
+    @PostMapping(value = "/user")
     public User add(){
         userDao.add(new User("1","heejung","1111"));
         return userDao.getById("1");
+    }
+
+    @DeleteMapping(value = "/users")
+    public String deleteAll(){
+        userDao.deleteAll();
+        return "delete All users"
     }
 }
