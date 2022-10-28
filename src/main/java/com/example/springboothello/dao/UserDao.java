0f679a9ef55bd409773e844fb6307bb6a93a3814
heejeong <hjx66@naver.com>
2022-10-28 13:38:00 +0900

    [feat] UserDao getById() 메소드 구현

--- src/main/java/com/example/springboothello/dao/UserDao.java
+++ src/main/java/com/example/springboothello/dao/UserDao.java
@@ -2,15 +2,18 @@ package com.example.springboothello.dao;
 
 import com.example.springboothello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+@Component
 public class UserDao {
-    private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -23,6 +26,15 @@ public class UserDao {
         this.jdbcTemplate.update("DELETE FROM users");
     }
 
-
+    public User getById(String id){
+        RowMapper<User> rowMapper = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id", rs.getString("name"), rs.getString("password")));
+                return user;
+            }
+        };
+        return this.jdbcTemplate.queryForObject("SELECT * FROM users where id = ?", rowMapper);
+    }
 
 }
