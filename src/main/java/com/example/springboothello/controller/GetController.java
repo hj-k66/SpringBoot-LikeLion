d00a271396c8d631ce953401f1e6d68b618b43cb
heejeong <hjx66@naver.com>
2022-10-28 11:38:19 +0900

    [add]로그 출력

--- src/main/java/com/example/springboothello/controller/GetController.java
+++ src/main/java/com/example/springboothello/controller/GetController.java
@@ -1,35 +1,42 @@
 package com.example.springboothello.controller;
 
 import com.example.springboothello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName(){
+        log.info("getName으로 요청이 들어왔습니다.");
         return "heejung";
     }
 
     @GetMapping(value = "/variable/{variable}")
     public String getVariable(@PathVariable String variable){
+        log.info("getVariable로 요청이 들어왔습니다. variable:{}", variable);
         return variable;
     }
 
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2(@PathVariable("variable") String var){
+        log.info("getVariable2로 요청이 들어왔습니다. variable :{}", var);
         return var;
     }
 
     @GetMapping(value="/request1")
     public String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
+        log.info("getRequestParam1d으로 요청이 들어왔습니다. name:{},email:{},organization:{}", name, email, organization);
         return String.format("%s %s %s", name, email, organization);
     }
 
