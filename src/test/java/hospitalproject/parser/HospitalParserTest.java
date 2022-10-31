31519f6b3434129963ce2e0520181c5e741f2288
heejeong <hjx66@naver.com>
2022-10-31 15:32:29 +0900

    test: csv 전체 파일 잘 파싱하는지 test

--- src/test/java/hospitalproject/parser/HospitalParserTest.java
+++ src/test/java/hospitalproject/parser/HospitalParserTest.java
@@ -3,13 +3,30 @@ package hospitalproject.parser;
 import hospitalproject.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest(classes = ParserFactory.class)
 class HospitalParserTest {
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Test
+    @DisplayName("csv 전체 라인을 잘 파싱하는지 test")
+    void ReadByLine() throws IOException {
+        String filename = "C:\\Users\\user\\Downloads\\fulldata_01_01_02_P_의원.csv";
+        List<Hospital> hospitals = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitals.size() > 10000);
+    }
+
     @Test
     @DisplayName("csv 1줄을 Hospital 인스턴스로 잘 만드는지 Test")
     void convertToHospital(){
@@ -33,5 +50,6 @@ class HospitalParserTest {
         assertEquals(0, hospital.getTotalNumberOfBeds());
         assertEquals(52.29f, hospital.getTotalAreaSize());
     }
+
 
 }
