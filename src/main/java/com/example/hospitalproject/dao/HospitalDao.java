1ebdc7506a0aa774f9aa3b7db0de0c40546bc042
heejeong <hjx66@naver.com>
2022-11-01 15:18:46 +0900

    feat: HospitalDao findById()구현

--- src/main/java/com/example/hospitalproject/dao/HospitalDao.java
+++ src/main/java/com/example/hospitalproject/dao/HospitalDao.java
@@ -2,7 +2,11 @@ package com.example.hospitalproject.dao;
 
 import com.example.hospitalproject.domain.Hospital;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class HospitalDao {
@@ -12,17 +16,29 @@ public class HospitalDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void deleteAll(){
+    RowMapper<Hospital> rowMapper = (rs, rowNum) -> {
+        Hospital hospital = new Hospital();
+        hospital.setId(rs.getInt("id"));
+        hospital.setOpenServiceName(rs.getString("open_servie_name"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        return hospital;
+    };
+
+    public Hospital findById(int id) {
+        return this.jdbcTemplate.queryForObject("SELECT * from national_hospitals where id = ?", rowMapper, id);
+    }
+
+    public void deleteAll() {
         this.jdbcTemplate.update("delete from national_hospitals");
     }
 
-    public int getCount(){
+    public int getCount() {
         String sql = "SELECT count(id) from national_hospitals";
         return this.jdbcTemplate.queryForObject(sql, Integer.class);
     }
 
 
-    public void add(Hospital hospital){
+    public void add(Hospital hospital) {
         String sql = "INSERT INTO `national_hospitals` (`id`, `open_service_name`, `open_local_government_code`," +
                 " `management_number`, `license_date`, `business_status`," +
                 " `business_status_code`, `phone`, `full_address`," +
